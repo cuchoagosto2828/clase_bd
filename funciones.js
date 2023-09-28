0d102cee725814cd506ac27561bb7f78d30809c9
cuchoagosto2828 <sebabedo28@gmail.com>
2023-09-27 21:58:14 -0500

    nuevo

--- funciones.js
+++ funciones.js
@@ -1,3 +1,7 @@
 function saludar() {
-    alert('¡Hola, mundo!');
+    alert('¡Hola! Bienvenido a mi página web.');
 }
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previene el envío predeterminado del formulario
+    window.location.href = "plantilla.html"; // Redirige a la página "plantilla.html"
+});
