803f5f58090679d466223c2221effd75a7b2a90a
cuchoagosto2828 <sebabedo28@gmail.com>
2023-09-28 20:56:39 -0500

    clase

--- funciones.js
+++ funciones.js
@@ -1,7 +1,10 @@
 function saludar() {
     alert('¡Hola! Bienvenido a mi página web.');
 }
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Previene el envío predeterminado del formulario
-    window.location.href = "plantilla.html"; // Redirige a la página "plantilla.html"
-});
+function validar(){
+    var usu = document.getElementById("yourUsername");
+    var contra = document.getElementById("yourPassword");
+    console.log("usu "+ usu.value ); // Agrega esta línea para verificar
+
+    window.location.href = "index.php";
+}
